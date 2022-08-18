5072f5a7ad7c9bf796f7bc401ca822eca1e15619
Olena Pohybil <itlentus@gmail.com>
2022-08-18 19:40:35 +0300

    add suspense

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
 import BubbleCanvas from 'animation/bubbles';
 import React, { useState } from 'react';
+import { lazy, Suspense } from 'react';
 
 import styled from 'styled-components';
 
-import Header from './Header';
-import Details from './Details/Details';
-import Trends from './Trends/Trends';
+// import Header from './Header';
+// import Details from './Details/Details';
+// import Trends from './Trends/Trends';
+
+const Header = lazy(() => import('./Header/Header'));
+const Details = lazy(() => import('./Details/Details'));
+const Trends = lazy(() => import('./Trends/Trends'));
 
 export const App = () => {
   const [movieId, setMovieId] = useState('66732');
@@ -17,9 +22,11 @@ export const App = () => {
   return (
     <BubbleCanvas>
       <Container>
-        <Header />
-        <Details id={movieId} />
-        <Trends title={'Popular this week'} getId={getId}></Trends>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+          <Details id={movieId} />
+          <Trends title={'Popular this week'} getId={getId}></Trends>
+        </Suspense>
       </Container>
     </BubbleCanvas>
   );
